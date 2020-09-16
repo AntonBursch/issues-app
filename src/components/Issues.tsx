ff424f5ba840bcedc03996e838a2a35240da710d
Anton Bursch <antonphd@gmail.com>
2020-09-15 20:44:19 -0700

    adding it runs tests for App & Issues

--- src/components/Issues.tsx
+++ src/components/Issues.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import { useState } from 'react-bootstrap/node_modules/@types/react';
-import { FC } from 'react-bootstrap/node_modules/@types/react';
 import { Issue } from '../models/Issue';
 
 interface IssuesProps {
@@ -14,7 +12,7 @@ export const Issues: React.FC<IssuesProps> = (props: IssuesProps) => {
         <React.Fragment>
             <ul>
                 {
-                    props.issues.filter((issue: Issue) => {
+                    issues.filter((issue: Issue) => {
                         return issue.state === 'open'
                     }).map((issue: Issue, index: number) => {
                         return (
